cbca0efff88de644dd729b929b1babf52a9e1373
vikram10-6 <vikramayer19@gmail.com>
2024-02-16 17:13:26 +0530

    Connected to database and created env file to secure sensitive  data

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
 
 app.listen(3000, ()=>{
+   
     console.log('server is running on port 3000!!!')
 })
