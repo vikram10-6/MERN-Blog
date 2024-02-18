d61b1db23aa70f1daf579e11d8c3765c8d4198fa
vikram10-6 <vikramayer19@gmail.com>
2024-02-18 14:25:56 +0530

    Created sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,33 +1,60 @@
-import User from "../models/user.model.js"
-import bcryptjs from 'bcryptjs'
-import { errorHandler } from "../utils/error.js"
- 
-export const signup =async (req,resp,next) =>{
-    const {username,email,password} =req.body
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import  Jwt  from "jsonwebtoken";
 
-    if (
-        !username ||
-        !email ||
-        !password ||
-        username === '' ||
-        email === '' ||
-        password === ''
-      ) {
-        next(errorHandler(400, 'All feilds are required')) 
+export const signup = async (req, resp, next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All feilds are required"));
+  }
+
+  const hashedPasword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPasword,
+  });
+
+  try {
+    await newUser.save();
+    resp.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, resp, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All feilds are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
     }
-    
-    const hashedPasword = bcryptjs.hashSync(password,10)
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+    const token = Jwt.sign({ id: validUser._id },process.env.JWT_SECRET)
+    const {password:pass, ...rest} = validUser._doc
+    resp.status(200).cookie('accerss_token',token,{
+      httpOnly: true
+    }).json(rest)
 
-    const newUser = new User({
-        username,
-        email,
-        password : hashedPasword,
-    })
-
-   try {
-    await newUser.save()
-    resp.json('Signup successful')
-   } catch (error) {
-     next(error)
-   }
-}
+  } catch (error) {
+    next(error);
+  }
+};
